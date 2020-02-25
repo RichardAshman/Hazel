2aed6e57270953da76bc8fd67431a05781b3b642
RichardAshman <r.ashman@windowslive.com>
2020-02-25 16:29:39 +1300

    Added Event system

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
 
 namespace Hazel {
 
@@ -13,6 +15,9 @@ namespace Hazel {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		HZ_TRACE(e);
+
 		while (true);
 	}
 
---
+++ Hazel/src/Hazel/Events/Event.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "Hazel/Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Hazel {
+	/*
+	Events in Hazel are currently blocking, meaning when an event occurs it
+	immediately gets dispatched and must be delt with right then and there.
+	For the future, a better strategy might be to buffer events in an event 
+	bus and process them during the "evenmt" part of the update stage.
+	*/
+
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MosueMoved, MosuesScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication	= BIT(0),
+		EvnetCatogoryInput			= BIT(1),
+		EvnetCatogoryKeyboard		= BIT(2),
+		EvnetCatogoryMouse			= BIT(3),
+		EvnetCatogoryMouseButton	= BIT(4)
+	};
+
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+	class HAZEL_API Event
+	{
+		friend class EventDispatcher;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0; // This should only be used in debugging. change this and GetCategoryFlags
+		virtual int GetCategoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetCategoryFlags() & category;
+		}
+	protected:
+		bool m_Handled = false;
+	};
+
+	class EventDispatcher
+	{
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event)
+		{
+		}
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func) //T is any event type that is being dipatched
+		{
+			if (m_Event.GetEventType() == T::GetStaticType()) 
+			{
+				m_Event.m_Handled = func(*(T*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
+	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
+}
