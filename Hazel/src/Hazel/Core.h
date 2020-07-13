3f1d8887aebb512a6b696f1e9d0b31b0da7f35c2
RichardAshman <37858598+RichardAshman@users.noreply.github.com>
2020-07-14 11:58:37 +1200

    Added Ref and Scope

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #ifdef HZ_PLATFORM_WINDOWS
 	#if HZ_DYNAMIC_LINK
 		#ifdef HZ_BUILD_DLL
@@ -29,3 +31,19 @@
 #define BIT(x) (1 << x)
 
 #define HZ_BIND_EVENT_FN(fn) std::bind(&fn,this, std::placeholders::_1)
+
+namespace Hazel {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+	// When using shared (or unique) pointers the code will change
+	// From 
+	// std::shared_ptr<Shader>
+	// to
+	// Ref<Shader> or Hazel::Ref<Shader>
+	// So that if we need to change it sometime we can just change it here and everything will be changed.
+}
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -22,7 +22,7 @@ public:
 			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
-		std::shared_ptr<Hazel::VertexBuffer> vertexBuffer;
+		Hazel::Ref<Hazel::VertexBuffer> vertexBuffer;
 		vertexBuffer.reset(Hazel::VertexBuffer::Create(vertices, sizeof(vertices)));
 		// Create and set the vertex layout in the form of location(x,y,z) colourx4(R, G, B, A)
 		Hazel::BufferLayout layout = {
@@ -33,7 +33,7 @@ public:
 		m_VertexArray->AddVertexBuffer(vertexBuffer);
 
 		uint32_t indices[3] = { 0, 1, 2 };
-		std::shared_ptr<Hazel::IndexBuffer> indexBuffer;
+		Hazel::Ref<Hazel::IndexBuffer> indexBuffer;
 		indexBuffer.reset(Hazel::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 		m_VertexArray->SetIndexBuffer(indexBuffer);
 
@@ -45,14 +45,14 @@ public:
 			 0.5f,  0.5f, 0.0f,
 			-0.5f,  0.5f, 0.0f
 		};
-		std::shared_ptr<Hazel::VertexBuffer> squareVB;
+		Hazel::Ref<Hazel::VertexBuffer> squareVB;
 		squareVB.reset(Hazel::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
 		squareVB->SetLayout({
 			{Hazel::ShaderDataType::Float3, "a_Position"}
 			});
 		m_SquareVA->AddVertexBuffer(squareVB);
 		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
-		std::shared_ptr<Hazel::IndexBuffer> squareIB;
+		Hazel::Ref<Hazel::IndexBuffer> squareIB;
 		squareIB.reset(Hazel::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
 		m_SquareVA->SetIndexBuffer(squareIB);
 
@@ -220,32 +220,19 @@ public:
 	}
 	void OnEvent(Hazel::Event& event) override
 	{
-		/*Hazel::EventDispatcher dispatcher(event);
+		/*
+		Hazel::EventDispatcher dispatcher(event);
 		dispatcher.Dispatch<Hazel::KeyPressedEvent>(HZ_BIND_EVENT_FN(ExampleLayer::OnkeyPressedEvent));
-*/
-
-
-		//if (!event.Handled) {
-		//	HZ_TRACE("{0}", event);
-		//}
-
-
-		//if (event.GetEventType() == Hazel::EventType::KeyPressed)
-		//{
-		//	Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
-		//	if (e.GetKeyCode() == HZ_KEY_TAB)
-		//		HZ_TRACE("Tab key is pressed (event)!");
-		//	HZ_TRACE("{0}", (char)e.GetKeyCode());
-		//}
+		*/
 	}
 
 private:
 
-	std::shared_ptr<Hazel::Shader> m_Shader;
-	std::shared_ptr<Hazel::VertexArray> m_VertexArray;
+	Hazel::Ref<Hazel::Shader> m_Shader;
+	Hazel::Ref<Hazel::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Hazel::Shader> m_FlatColorShader;
-	std::shared_ptr<Hazel::VertexArray> m_SquareVA;
+	Hazel::Ref<Hazel::Shader> m_FlatColorShader;
+	Hazel::Ref<Hazel::VertexArray> m_SquareVA;
 
 	Hazel::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
